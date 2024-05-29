6ba1b19938761af5bea62c2b7f5893f8a36d0a13
Polybius93 <adam.daniel.puskas@polybius-studios.com>
2024-05-29 15:56:06 +0200

    feat: modify attestor handler ethereum chain id field

--- src/query-handlers/attestor-handler.ts
+++ src/query-handlers/attestor-handler.ts
@@ -1,26 +1,14 @@
 /** @format */
 
-import { ethereumArbitrum, ethereumArbitrumSepolia } from '../constants/ethereum-constants.js';
 import { AttestorError } from '../models/errors.js';
-import { EthereumNetwork } from '../models/ethereum-models.js';
 
 export class AttestorHandler {
   private attestorRootURLs: string[];
   private ethereumChainID: string;
 
-  constructor(attestorRootURLs: string[], ethereumNetwork: EthereumNetwork) {
-    this.attestorRootURLs = attestorRootURLs;
-
-    switch (ethereumNetwork) {
-      case ethereumArbitrum:
-        this.ethereumChainID = 'evm-arbitrum';
-        break;
-      case ethereumArbitrumSepolia:
-        this.ethereumChainID = 'evm-arbsepolia';
-        break;
-      default:
-        throw new Error(`Could not get Ethereum Chain ID: Ethereum Network not supported`);
-    }
+  constructor(attestorRootURLs: string[], ethereumChainID: 'evm-arbitrum' | 'evm-arbsepolia' | 'evm-localhost') {
+    this.attestorRootURLs = attestorRootURLs;   
+    this.ethereumChainID = ethereumChainID;
   }
 
   async createPSBTEvent(
@@ -31,17 +19,19 @@ export class AttestorHandler {
   ): Promise<void> {
     const createPSBTEndpoints = this.attestorRootURLs.map((url) => `${url}/app/create-psbt-event`);
 
+    const body = JSON.stringify({
+        uuid: vaultUUID,
+        funding_transaction: fundingTransaction,
+        closing_psbt: closingPSBT,
+        mint_address: userNativeSegwitAddress,
+        chain: this.ethereumChainID,
+        });
+
     const requests = createPSBTEndpoints.map(async (url) =>
       fetch(url, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
-        body: JSON.stringify({
-          uuid: vaultUUID,
-          funding_transaction: fundingTransaction,
-          closing_psbt: closingPSBT,
-          mint_address: userNativeSegwitAddress,
-          chain: this.ethereumChainID,
-        }),
+        body
       })
         .then((response) => (response.ok ? true : response.statusText))
         .catch((error) => error.message)
