5850ef97ac088e93641a6448dafdc98354c59a69
Ksenia Kulikova <34596539+kskulikova@users.noreply.github.com>
2024-08-01 15:31:19 +0200

    feat: add PsbtEvent to attestor models (#23)
    * feat: add PsbtEvent to attestor models
    
    * chore: incrementing the version
    
    ---------
    
    Co-authored-by: ks <ksenia@dlc.link>

--- src/models/attestor.models.ts
+++ src/models/attestor.models.ts
@@ -17,3 +17,15 @@ export interface WithdrawDepositTXAttestorInfo {
   vaultUUID: string;
   withdrawDepositPSBT: string;
 }
+
+export interface PsbtEvent {
+  closing_psbt: string;
+  wd_psbt: string;
+  mint_address: string;
+  uuid: string;
+  funding_txid: string;
+  closing_txid: string;
+  status: string;
+  chain_name: string;
+  user_pubkey: string;
+}
