7a8f62efb9eba136e24b7e9d20bce83398510216
Polybius93 <adam.daniel.puskas@polybius-studios.com>
2025-03-03 15:40:32 +0100

    chore: bump version

--- src/functions/proof-of-reserve/proof-of-reserve-functions.ts
+++ src/functions/proof-of-reserve/proof-of-reserve-functions.ts
@@ -15,13 +15,13 @@ import {
  * Verifies the deposit of a vault by checking the transaction ID and confirming the transaction,
  * then returns the value of the vault's output in the transaction in satoshis.
  *
- * @param vault - The vault object containing transaction IDs and other relevant data.
- * @param extendedAttestorGroupPublicKey - The extended public key of the attestor group.
- * @param bitcoinBlockchainBlockHeight - The current block height of the Bitcoin blockchain.
- * @param bitcoinBlockchainAPI - The API endpoint to fetch Bitcoin transactions.
- * @param bitcoinNetwork - The Bitcoin network configuration (mainnet or testnet).
- * @returns A promise that resolves to the value of the vault's output in the transaction in satoshis, or 0 if the transaction is not confirmed or invalid.
- * @throws Will log an error message if there is an issue verifying the vault deposit.
+ * @param vault - The vault object containing transaction IDs and other relevant data
+ * @param extendedAttestorGroupPublicKey - The extended public key of the attestor group
+ * @param bitcoinBlockchainBlockHeight - The current block height of the Bitcoin blockchain
+ * @param bitcoinBlockchainAPI - The URL of the Bitcoin blockchain API
+ * @param bitcoinNetwork - The Bitcoin network to use
+ * @returns A promise that resolves to the value of the vault's output in the transaction in satoshis, or 0 if the transaction is not confirmed or invalid
+ * @throws Will log an error message if there is an issue verifying the vault deposit
  */
 export async function getVaultDepositValue(
   vault: RawVault,
